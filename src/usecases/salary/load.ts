1dae12766e07c4ab1365ae9a68cfb956780a6634
Lucas Oliveira <lucasnhso@gmail.com>
2023-10-25 16:22:19 -0300

    add treatments on loadSalary

--- src/usecases/salary/load.ts
+++ src/usecases/salary/load.ts
@@ -2,6 +2,18 @@ import { prismaClient } from "../../database";
 
 export async function loadSalary() {
   const data = await prismaClient.salary.findMany();
-
-  return data;
+  const treatedData = data.map((item) => {
+    const discounts = item.discounts as any[];
+    const discountTotal = discounts.reduce(
+      (acc: number, item: any) => acc + item.value,
+      0
+    );
+    return {
+      ...item,
+      receivedValue: item.receivedValue + item.advanceValue,
+      discountTotal,
+      discounts: undefined,
+    };
+  });
+  return treatedData;
 }
